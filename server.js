f5a1661b17a8e96b9fdf68ae708aeb4c610b442e
Munkhsoyol <munkhsoyoldavaa@gmail.com>
2026-03-31 15:46:31 +0800

    17

--- server.js
+++ server.js
@@ -5,8 +5,39 @@ dotenv.config({ path: "./config/config.env" });
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hello from Express server!");
+app.get("/api/v1/categories", (req, res) => {
+    res.status(200).json({
+        success: true,
+        data: "View category."
+    });
+});
+
+app.get("/api/v1/categories/:id", (req, res) => {
+    res.status(200).json({
+        success: true,
+        data: `${req.params.id} ID show category.`
+    });
+});
+
+app.post("/api/v1/categories", (req, res) => {
+    res.status(200).json({
+        success: true,
+        data: "Create category."
+    });
+});
+
+app.put("/api/v1/categories/:id", (req, res) => {
+    res.status(200).json({
+        success: true,
+        data: `${req.params.id} ID change category.`
+    });
+});
+
+app.delete("/api/v1/categories/:id", (req, res) => {
+    res.status(200).json({
+        success: true,
+        data: `${req.params.id} ID delete category.`
+    });
 });
 
 app.listen(process.env.PORT, console.log(`Express сервер ${process.env.PORT} порт дээр аслаа....`));
