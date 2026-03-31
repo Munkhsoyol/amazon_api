827cebf414e6194c740bc559e41a736ce5277b5b
Munkhsoyol <munkhsoyoldavaa@gmail.com>
2026-03-31 13:26:06 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config/config.env" });
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello from Express server!");
+});
+
+app.listen(process.env.PORT, console.log(`Express сервер ${process.env.PORT} порт дээр аслаа....`));
